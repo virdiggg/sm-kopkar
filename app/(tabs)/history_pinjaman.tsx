41b7a9102c0d5dd99a5ea5758ddf60e7a9355b32
Abang Goyang <virdigunawann@gmail.com>
2024-12-14 19:15:15 +0700

    update

--- app/(tabs)/history_pinjaman.tsx
+++ app/(tabs)/history_pinjaman.tsx
@@ -1,24 +1,29 @@
-import { StyleSheet, TouchableOpacity, Dimensions } from 'react-native';
+import { StyleSheet, TouchableOpacity, Dimensions } from "react-native";
 import { Text, ActivityIndicator, TextInput } from "react-native-paper";
-import { ThemedView } from '@/components/ThemedView';
+import { ThemedView } from "@/components/ThemedView";
 import React, { useEffect, useState } from "react";
-import { router } from 'expo-router';
+import { router } from "expo-router";
 import { useRouter, Stack } from "expo-router";
-import { isSignedIn, signOut } from '@/services/auth';
+import { isSignedIn, signOut } from "@/services/auth";
 import { fetchWithRetry } from "@/services/fetching";
 import { styles as glStyles } from "@/assets/styles";
 import Header from "@/components/Header";
 
 const { width, height } = Dimensions.get("window"); // Get screen dimensions
 
-export default function ProfileScreen() {
-    const [errors, setErrors] = useState('');
+export default function HistoryPinjamanScreen() {
+    const [errors, setErrors] = useState({});
+    const [errorMsg, setErrorMsg] = useState('');
     const [user, setUser] = useState({});
     const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
-        setErrors('');
-        setUser('');
+        setErrors({});
+        // setErrorMsg("");
+        // setJumlahPinjaman("");
+        // setLamaAngsuran("");
+        // setIsSubmit(false);
+
         setIsLoading(true);
 
         const checkIfSignedIn = async () => {
@@ -28,48 +33,24 @@ export default function ProfileScreen() {
                 return;
             }
 
-            try {
-                // Refresh token jadi gak expired
-                const response = await fetchWithRetry(`user/my-profile`, {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                });
-
-                // Kalo bukan 200 berarti token invalid,
-                // jadi hapus aja token dari storage, terus redirect ke login
-                if (response && response.statusCode !== 200) {
-                    signOut();
-                    router.replace("/login");
-                    return;
-                }
-
-                // Simpan token baru di storage
-                setUser(response.data);
-
-            } catch (error: any) {
-                // console.log("Error:", error);
-                // setErrors(error);
-            } finally {
-                // setTimeout(() => {
-                //   setIsLoading(false);
-                // }, 3000);
-                setIsLoading(false);
-            }
+            setIsLoading(false);
         };
 
         checkIfSignedIn();
+
         return () => {
-            setErrors('');
-            setUser('');
-            setIsLoading(false);
+            setErrors({});
+            //   setErrorMsg('');
+            //   setJumlahPinjaman('');
+            //   setLamaAngsuran('');
+            //   setIsLoading(false);
+            //   setIsSubmit(false);
         };
     }, []);
 
     const goBack = () => {
         router.back();
-    }
+    };
 
     if (isLoading) {
         return (
@@ -82,21 +63,33 @@ export default function ProfileScreen() {
 
     return (
         <ThemedView style={glStyles.container}>
-            <Stack.Screen options={{
-                headerShown: true,
-                headerRight: () => (
-                    <TouchableOpacity onPress={goBack}>
-                        <Text style={[glStyles.buttonTextTheme, { paddingHorizontal: 10 }]}>Back</Text>
-                    </TouchableOpacity>
-                )
-            }} />
+            <Stack.Screen
+                options={{
+                    headerShown: true,
+                    headerRight: () => (
+                        <TouchableOpacity onPress={goBack}>
+                            <Text
+                                style={[glStyles.buttonTextTheme, { paddingHorizontal: 10 }]}
+                            >
+                                Back
+                            </Text>
+                        </TouchableOpacity>
+                    ),
+                }}
+            />
             <ThemedView style={styles.grid}>
-                <Header
-                    title={`Profile`}
-                    subtitle="Anggota Koperasi Karyawan"
-                />
+                <Header title={`Profile`} subtitle="Anggota Koperasi Karyawan" />
                 <ThemedView style={styles.row}>
-                    <Text style={[glStyles.buttonTextTheme, glStyles.textBold, glStyles.textCenter, { fontSize: 20 }]}>Nama:</Text>
+                    <Text
+                        style={[
+                            glStyles.buttonTextTheme,
+                            glStyles.textBold,
+                            glStyles.textCenter,
+                            { fontSize: 20 },
+                        ]}
+                    >
+                        Nama:
+                    </Text>
                 </ThemedView>
                 <ThemedView style={styles.row}>
                     <TextInput
@@ -104,11 +97,20 @@ export default function ProfileScreen() {
                         keyboardType="default"
                         style={styles.input}
                         disabled={true}
-                        autoCapitalize='none'
+                        autoCapitalize="none"
                     />
                 </ThemedView>
                 <ThemedView style={styles.row}>
-                    <Text style={[glStyles.buttonTextTheme, glStyles.textBold, glStyles.textCenter, { fontSize: 20 }]}>Kode Toko:</Text>
+                    <Text
+                        style={[
+                            glStyles.buttonTextTheme,
+                            glStyles.textBold,
+                            glStyles.textCenter,
+                            { fontSize: 20 },
+                        ]}
+                    >
+                        Kode Toko:
+                    </Text>
                 </ThemedView>
                 <ThemedView style={styles.row}>
                     <TextInput
@@ -116,11 +118,20 @@ export default function ProfileScreen() {
                         keyboardType="default"
                         style={styles.input}
                         disabled={true}
-                        autoCapitalize='none'
+                        autoCapitalize="none"
                     />
                 </ThemedView>
                 <ThemedView style={styles.row}>
-                    <Text style={[glStyles.buttonTextTheme, glStyles.textBold, glStyles.textCenter, { fontSize: 20 }]}>Nama Bank:</Text>
+                    <Text
+                        style={[
+                            glStyles.buttonTextTheme,
+                            glStyles.textBold,
+                            glStyles.textCenter,
+                            { fontSize: 20 },
+                        ]}
+                    >
+                        Nama Bank:
+                    </Text>
                 </ThemedView>
                 <ThemedView style={styles.row}>
                     <TextInput
@@ -128,11 +139,20 @@ export default function ProfileScreen() {
                         keyboardType="default"
                         style={styles.input}
                         disabled={true}
-                        autoCapitalize='none'
+                        autoCapitalize="none"
                     />
                 </ThemedView>
                 <ThemedView style={styles.row}>
-                    <Text style={[glStyles.buttonTextTheme, glStyles.textBold, glStyles.textCenter, { fontSize: 20 }]}>No. Rekening:</Text>
+                    <Text
+                        style={[
+                            glStyles.buttonTextTheme,
+                            glStyles.textBold,
+                            glStyles.textCenter,
+                            { fontSize: 20 },
+                        ]}
+                    >
+                        No. Rekening:
+                    </Text>
                 </ThemedView>
                 <ThemedView style={styles.row}>
                     <TextInput
@@ -140,11 +160,20 @@ export default function ProfileScreen() {
                         keyboardType="default"
                         style={styles.input}
                         disabled={true}
-                        autoCapitalize='none'
+                        autoCapitalize="none"
                     />
                 </ThemedView>
                 <ThemedView style={styles.row}>
-                    <Text style={[glStyles.buttonTextTheme, glStyles.textBold, glStyles.textCenter, { fontSize: 20 }]}>ID Koperasi:</Text>
+                    <Text
+                        style={[
+                            glStyles.buttonTextTheme,
+                            glStyles.textBold,
+                            glStyles.textCenter,
+                            { fontSize: 20 },
+                        ]}
+                    >
+                        ID Koperasi:
+                    </Text>
                 </ThemedView>
                 <ThemedView style={styles.row}>
                     <TextInput
@@ -152,7 +181,7 @@ export default function ProfileScreen() {
                         keyboardType="default"
                         style={styles.input}
                         disabled={true}
-                        autoCapitalize='none'
+                        autoCapitalize="none"
                     />
                 </ThemedView>
             </ThemedView>
@@ -163,14 +192,14 @@ export default function ProfileScreen() {
 const styles = StyleSheet.create({
     grid: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        justifyContent: "center",
+        alignItems: "center",
     },
     row: {
         flex: 1,
-        flexDirection: 'row',
-        backgroundColor: '#fff',
-        width: '100%',
+        flexDirection: "row",
+        backgroundColor: "#fff",
+        width: "100%",
         paddingHorizontal: width * 0.05,
     },
     input: {
